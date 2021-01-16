5866e13279786d70a7394d53c44635fb2308e258
Lxr82 <chi.kevin.cheng2002@gmail.com>
2021-01-16 00:41:16 -0500

    updated functionality in main

--- app/src/main/java/li/raymond/whatsinmyfood/AnalyzeActivity.java
+++ app/src/main/java/li/raymond/whatsinmyfood/AnalyzeActivity.java
@@ -2,13 +2,34 @@ package li.raymond.whatsinmyfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.io.IOException;
 
 public class AnalyzeActivity extends AppCompatActivity {
+	Uri imageUri;
+	InputImage image;
+	Context context = getApplicationContext();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_analyze);
+
+		ImageView image = (ImageView) findViewById(R.id.DisplayImage);
+
+		Intent intent = getIntent();
+		String stringUri = intent.getExtras().getString("imageUri");
+		imageUri = Uri.parse(stringUri);
+		image.setImageURI(imageUri);
+	}
+
+	public void analyze() throws IOException {
+		image = InputImage.fromFilePath(context, imageUri);
 	}
 }
