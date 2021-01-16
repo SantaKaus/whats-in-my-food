c9024b3d8b89fdf85df1800dd57d3cc0439fc462
Lxr82 <chi.kevin.cheng2002@gmail.com>
2021-01-16 11:19:09 -0500

    added regex

--- app/src/main/java/li/raymond/whatsinmyfood/AnalyzeActivity.java
+++ app/src/main/java/li/raymond/whatsinmyfood/AnalyzeActivity.java
@@ -6,24 +6,19 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.SparseArray;
-import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
-
-
-
-import com.google.mlkit.vision.common.InputImage;
-
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class AnalyzeActivity extends AppCompatActivity {
 	Uri imageUri;
 	TextView textView;
 	TextRecognizer recognizer;
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +26,10 @@ public class AnalyzeActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_analyze);
 
 		textView = findViewById(R.id.textView);
-
 		Intent intent = getIntent();
 		String imagePath = intent.getExtras().getString("imageUri");
 		imageUri = Uri.parse(imagePath);
+
 		try {
 			runTextRecognition();
 		} catch (IOException e) {
@@ -46,26 +41,32 @@ public class AnalyzeActivity extends AppCompatActivity {
 		recognizer =  new TextRecognizer.Builder(getApplicationContext()).build();
 		Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
 
-
-		if(recognizer.isOperational())
-		{
-
-			// bitmap.;
+		if(recognizer.isOperational()) {
 			Frame frame = new Frame.Builder().setBitmap(bitmap).build();
 
 			final SparseArray<TextBlock> items = recognizer.detect(frame);
-			if (items.size() != 0)
-			{
+			if (items.size() != 0) {
 				StringBuilder stringBuilder = new StringBuilder();
-				for (int i=0 ; i<items.size(); i++)
-				{
+				for (int i=0 ; i<items.size(); i++) {
 					TextBlock item = items.valueAt(i);
 					stringBuilder.append(item.getValue());
-					stringBuilder.append("\n");
 				}
-				textView.setText(stringBuilder.toString());
+				createTable(stringBuilder.toString().toLowerCase());
 			}
 
 		}
 	}
+
+	private void createTable(String basicResult) {
+		String formattedResult = basicResult.toLowerCase()
+			.split("ingredients: ")[1].replaceAll("[^a-zA-Z0-9\\s]", " ");
+		String[] ingredientList = formattedResult.split("  ");
+		populateTable(ingredientList);
+	}
+
+	private void populateTable(String[] iList) {
+
+	}
+
+
 }
