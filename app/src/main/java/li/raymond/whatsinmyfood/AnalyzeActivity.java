3b21a2281f638e276e9e7ef7b43accc7a8c87c95
Raymond Li <hi@raymond.li>
2021-01-16 23:30:57 -0500

    Update AnalyzeActivity.java

--- app/src/main/java/li/raymond/whatsinmyfood/AnalyzeActivity.java
+++ app/src/main/java/li/raymond/whatsinmyfood/AnalyzeActivity.java
@@ -61,38 +61,41 @@ public class AnalyzeActivity extends AppCompatActivity {
 					stringBuilder.append(item.getValue());
 				}
 				// Can swap between keyIngredients and allIngredients
-				createTable(keyIngredients(stringBuilder.toString().toLowerCase()));
+				createTable(keyIngredients(stringBuilder.toString().toLowerCase().split("ingredients: ")[1].replaceAll("[^a-zA-Z0-9\\-\\s]", " ").replaceAll("[ \t\n\r]*", " ")));
 			}
 		}
 	}
 
 	private String[] allIngredients(String basicResult) {
-		String formattedResult = basicResult.toLowerCase()
-				.split("ingredients: ")[1].replaceAll("[^a-zA-Z0-9\\-\\s]", " ").replaceAll("[ \t\n\r]*", " ");
-
-		String[] ingredientList = formattedResult.split(" {2}");
+		String[] ingredientList = basicResult.split(" {2}");
 		for (int i = 0; i < ingredientList.length; i++) {
 			Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(ingredientList[i]);
-			if (m.find()) {
+			if (m.find())
 				ingredientList[i] = m.group(1);
-			}
 		}
 		return ingredientList;
 	}
 
 	private Map<String, String> ingredients = Stream.of(new String[][]{
-			{"Soy lecithin", ""},
-			{"Polyglycerol", "Doe"},
-			{"polyricinoleate", ""}
+			{"Soy lecithin", "Emulsifier (helps things mix) and lubricant. Mostly harmless since quantities are very small. Can be safely consumed by people with soy allergies. May reduce cholesterol and provides Choline (an essential nutrient). Generally recognized as safe (GRAS)."},
+			{"Polyglycerol", "Polyglycerol polyricinoleate (PGPR) is an emulsifier (helps things mix) used with soy lecithin to reduce viscosity (thickness) in chocolates, spreads and salad dressings. Improves texture of baked goods. Generally recognized as safe (GRAS) when consumed in small amounts."},
+			{"Maltodextrin", "Eumlsifier (helps things mix). Improves flavor, thickness, and shelf life of food. Carbohydrate supplement. Comes from starch like potatoes. Provides energy without nutrition. May increase cholesterol when consumed in large amounts. Generally recognized as safe (GRAS)."},
+			{"Citric acid", "The sour part of lemon juice. Provides flavor and improves shelf life. One of the most common food additives. Added to canned foods to protect against botulism (dangerous disease). Helps the body transform food into energy. Dissolves kidney stones. Generally recognized as safe (GRAS)."},
+			{"Monosodium glutamate", "MSG enhances the umami (savory meaty flavor) of foods. Popular in Asian cooking. Stimulates nervous system, making you feel full without eating much. May cause weight gain and asthma attacks, and a dry mouth/throat. Safe in small amounts, harmful in large amounts."},
+			{"Red 40 lake", "Allura red synthetic dye made from petroleum (car gas). Adds color. Commonly found in dairy products, sweets, snacks, baked goods, and beverages. May cause allergies and worsen behavior of children with ADHD. Safe in small amounts."},
+			{"Yellow 6 lake", "Sunset yellow dye. Adds color. Used in candy, sauces, baked goods and preserved fruits. May cause allergies and worsen behavior of children with ADHD. Safe in small amounts."},
+			{"Sodium bicarbonate", "Baking soda. Helps baked goods rise. Safe."},
+			{"Disodium inosinate", "Umami (savory meaty flavor) enhancer. Used to avoid putting MSG in the ingredients list. Common in fast foods, including pizza and snacks like potato chips. Some allergies and side effects have been reported. Generally recognized as safe (GRAS)."},
+			{"Disodium guanylate", "Amplifies salt's salty taste. Commonly combined with MSG to enhance food flavor. Found in processed foods. Considered safe, but acceptable quantity of intake has yet to be determined due to lack of research."},
+			{"TBHQ", "Tertiary butylhydroquinone. Acts as an antioxidant but not natural. Extends shelf life of food (preservative). May damage liver and nervous system. Safety is questionable and banned in many countries."}
 	}).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 
 	private String[] keyIngredients(String basicResult) {
+		basicResult = basicResult.replaceAll("\\s+", "");
 		ArrayList<String> ingredientList = new ArrayList<>();
-		for (String ingredient : ingredients.keySet()) {
-			if (basicResult.contains(ingredient.toLowerCase())) {
+		for (String ingredient : ingredients.keySet())
+			if (basicResult.contains(ingredient.toLowerCase()))
 				ingredientList.add(ingredient);
-			}
-		}
 		alertDialog("2", basicResult);
 		return ingredientList.toArray(new String[0]);
 	}
